b8185d4614551f8e406d94b7782ad0e212ebfa58
DESKTOP-N3U3U1N\hli2 <hli2@tcs.woolworths.com.au>
2022-10-26 14:41:23 +1100

    fetch api

---
+++ app.js
@@ -0,0 +1,28 @@
+
+
+const process = require('process')
+const url ='https://api.publicapis.org/entries';
+// GET Request.
+
+var arg = process.argv.slice(2,3)
+var limit = process.argv.slice(3,4)
+console.log('arg:',arg)
+console.log('limit:',limit)
+
+
+fetch(`${url}?category=${arg}`)
+    // Handle success
+    .then(response => response.json())  // convert to json
+    .then(
+        
+            (json) => {
+                json.entries? json.entries.slice(0,limit).forEach(element => {
+                    console.log(element)
+                }
+                ):console.log('no result')
+            }
+            
+        
+        
+    )    //print data to console
+    .catch(err => console.log('Request Failed', err)); // Catch errors
