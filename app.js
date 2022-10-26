af7fc0c6d897e93e16b84d55bf4f3d2d9935f947
DESKTOP-N3U3U1N\hli2 <hli2@tcs.woolworths.com.au>
2022-10-26 16:21:26 +1100

    add sql file

--- app.js
+++ app.js
@@ -11,15 +11,11 @@ fetch(`${url}?category=${arg}`)
     // Handle success
     .then(response => response.json())  // convert to json
     .then(
-        
             (json) => {
                 json.entries? json.entries.slice(0,limit).forEach(element => {
                     console.log(element)
                 }
                 ):console.log('no result')
             }
-            
-        
-        
     )    //print data to console
     .catch(err => console.log('Request Failed', err)); // Catch errors
